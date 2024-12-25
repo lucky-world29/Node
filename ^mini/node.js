fb7b3d44353d710dcbf81efc1572615f5f33cf51
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-12-26 00:01:04 +0530

    Update node.js

--- ^mini/node.js
+++ ^mini/node.js
@@ -6,6 +6,8 @@ ES6 introduced classes.
 
 A class is a type of function, but instead of using 
 the keyword function to initiate it, we use the keyword class, 
+
+
 and the properties are assigned inside a constructor() method.
    */
 
